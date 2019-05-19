d29fa0ef32579a3981546e7c08888d94e75afc29
John Fajardo <fajardocj@gmail.com>
2019-05-19 01:14:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,7 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma(string){}
+function sayHiToGrandma(string){
+  if string != string.toUpperCase
+  return "I can\'t hear you"
+}
